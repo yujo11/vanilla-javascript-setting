c7ac50cbe3195e1a297bb1061666cc4f5a18cff1
yujo <yujo@student.42seoul.kr>
2020-12-16 19:53:06 +0900

    test: eslint & prettier run test

---
+++ test.js
@@ -0,0 +1,27 @@
+const testMaxDepth = () => {
+  if (1) {
+    if (1) {
+      if (1) {
+      }
+    }
+  }
+};
+
+const testMaxLines = () => {
+  let test = 1;
+  test = 3;
+  test = 2;
+  test = 4;
+  test = 5;
+  test = 6;
+  test = 7;
+  test = 8;
+  test = 9;
+  test = 10;
+  test = 11;
+  test = 12;
+  test = 13;
+  test = 14;
+  test = 15;
+  test = 16;
+};
